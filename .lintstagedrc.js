45195ff4934637c5654bcb43cd980eb3945078f5
daliskafroyan <firstaroyand.paper@gmail.com>
2021-12-10 01:17:48 +0700

    test: deleting lint staged 2

--- .lintstagedrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // Run type-check on changes to TypeScript files
-    '**/*.(tsx|jsx|js|ts)?(x)': () => 'yarn type-check',
-    // Run ESLint on changes to JavaScript/TypeScript files
-    '**/*.(tsx|jsx|js|ts)?(x)': (filenames) =>
-        `yarn lint . ${filenames.join(' ')}`,
-};
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,9 +14,10 @@ const Home: NextPage = () => {
                 />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
-            <h2 tw='bg-yellow-300 font-bold p-3 md:text-2xl'>
+            <h2 tw='p-3 font-bold bg-yellow-300 md:text-2xl'>
                 Hi! Welcome to your first Next.js siteaaaaaaaa.
             </h2>
+            <Image src='/vercel.svg' width={72} height={16} />
 
             <main className={styles.main}>
                 <h1 className={styles.title}>
